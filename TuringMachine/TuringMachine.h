bd6ea9369a7a6661679c62b85afc550f1e208c03
Lilit Bareghamyan <100960159+lbareghamyan@users.noreply.github.com>
2022-04-29 00:09:56 +0400

    Update TuringMachine.h

--- TuringMachine/TuringMachine.h
+++ TuringMachine/TuringMachine.h
@@ -10,34 +10,31 @@ class TuringMachine
 	std::deque<char> tape;
 
 	std::unordered_set<std::string> Q; //set of states
-	std::string* tempQ;
+	std::string* tempQ; // temp string arr for keeping input order so the transition table can be made
 
-	std::unordered_set<char> T; //T is the tape alphabet 
-								//(symbols which can be written on Tape)
+	std::unordered_set<char> T; //T is the tape alphabet (symbols which can be written on Tape)
 	char* tempT;
 
-
-	char b = 'B'; //B is blank symbol (every cell is filled with B except input alphabet initially)
-	std::unordered_set<char> Sigma;//Sigma is the input alphabet 
-											//(symbols which are part of input alphabet)
-	std::unordered_set<std::string> F;//F is the set of final states. 
-										//If any state of F is reached, input string is accepted.
+	std::unordered_set<char> Sigma;//Sigma is the input alphabet (symbols which are part of input alphabet)
+	std::unordered_set<std::string> F;//F is the set of final states. If any state of F is reached, input string is accepted.
 
 	std::map<std::pair<std::string, char>,
 		std::pair<std::pair<std::string, char>, char>> transitionsTable;
 
-
+	// writes into Q, T, Sigma or F according to dest value 
 	bool writeInto(std::string, int k);
 	
 	bool isIn(const std::unordered_set<std::string>,
 		const std::unordered_set<std::string>)const;
 	bool isIn(const std::unordered_set<char>,
 		const std::unordered_set<char>)const;
-
+	
+	//returns true if string is valid cell of TT and writes values in q t and dir
 	bool cellIsValid(std::string, std::string& q, char& t, char& dir);
-
+	
+	//checks if input string is valid, if so writes it into tape
 	bool inputIsValid(std::string);
-
+	
 	void printTape();
 
 public:
